0972dc66929a4b0a39e79dcbd63f68d3d49796d5
ATorrecilla <a.torrecilla@alumnos.upm.es>
2018-10-02 21:27:17 +0200

    commit issue#4 clase Point metodos set get clase PointTest metodo testSetn

--- src/main/java/es/upm/miw/iwvg/forge/junit/Point.java
+++ src/main/java/es/upm/miw/iwvg/forge/junit/Point.java
@@ -6,17 +6,25 @@ public class Point {
 
     private int y;
 
+    private int z;
+
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     public Point(int xy) {
-        this(xy, xy);
+        this(xy, xy, xy);
     }
 
     public Point() {
-        this(0, 0);
+        this(0, 0, 0);
     }
 
     public double module() {
@@ -38,6 +46,22 @@ public class Point {
 
     public int getY() {
         return this.y;
+    }
+
+    public int getZ() {
+        return this.z;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
     }
 
     @Override
